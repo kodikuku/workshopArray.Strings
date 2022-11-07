3bd7b5fdb81e26bb947e6ec6b85051b97655d404
kodikuku <kodibotrus@gmail.com>
2022-11-07 15:01:46 +0100

    APIEX5

---
+++ Date.time/src/main/java/EX10.java
@@ -0,0 +1,12 @@
+import java.time.*;
+
+public class EX10 {
+
+    public static void main(String[] args) {
+        LocalTime currentTime1 = local time.now();
+        System.out.println("currenttime);
+
+    }
+
+  }
+
